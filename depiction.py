5b01164b3f5790a878a116984002e3f1763b421f
qoolaaq <qoolaaq@gmail.com>
2020-01-04 01:38:02 +0900

    つづき

--- depiction.py
+++ depiction.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-from pygame.locals import  QUIT, Rect
+from pygame.locals import  QUIT, Rect, MOUSEBUTTONDOWN
 
 pygame.init()
 # pygameを使うアプリでは必ず最初に呼び出す必要がある
@@ -17,18 +17,27 @@ FPSCLOCK = pygame.time.Clock()
 def main():
     """main routine"""
     sysfont = pygame.font.SysFont(None, 50)
+    mousepos = []
+    # ここにマウスがクリックされたときの座標情報がタプルとして入る
 
     while True:
-        for event in pygame.event.get(QUIT):
+        SURFACE.fill((255, 255, 255))
+        # SURFACEの中身をタプル()で塗りつぶす
+        # (R,G,B)
+        # (255,255,255)で白
+
+        for event in pygame.event.get():
         # イベントキューからイベントを取得する
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type == MOUSEBUTTONDOWN:
+                mousepos.append(event.pos)
+                # マウスの座標＝event.posでタプルとして取得
 
-        SURFACE.fill((0, 0, 0))
-        # SURFACEの中身をタプル()で塗りつぶす
-        # (R,G,B)
-        # (255,255,255)で白
+        for i, j in mousepos:
+            pygame.draw.circle(SURFACE, (0, 255, 0), (i, j), 5)
+        # mouseposから座標の情報を引っ張ってきて、円を描写
 
         for xpos in range(0, 400, 25):
             pygame.draw.line(SURFACE, 0xFFFFF, (xpos, 0), (xpos, 300))

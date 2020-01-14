bc40c2339e1a903858638006720f232a1b6f371a
qoolaaq <qoolaaq@gmail.com>
2020-01-15 00:26:40 +0900

    SkillProcessorの簡単なテストを行った。上手く行きそうなのでこれでいく。

--- skill.py
+++ skill.py
@@ -2,10 +2,19 @@ class Skill:
     """
     各ユニットに持たせるインスタンス
     skill_of_Name で生成する予定
+        発動のタイミング
+            登場時(CIF: come into field)
+            退場時(PIO: placed into outside)
+            その他条件(T: trigered)
+        ...みたいな感じ
+        boolで持たせるべきかも
+
     """
     def __init__(self):
+        self.timing_test = False
+        self.test = False
         pass
-    def effect(self, unit):
+    def effect(unit):
         """
         unitを引数にする
         実際にはGameControllerで動かすので、
--- skill_processor.py
+++ skill_processor.py
@@ -1,8 +1,50 @@
-# from skill import *
+from skill import *
 
-class SkillProcessor(list):
+class SkillProcessor():
     """
     skillを入れて、順次処理していく
     """
     def __init__(self):
+        self.list = []
         pass
+    def load_skill(self, skill):
+        """
+        スキルを解決するリストに載せておく
+        """
+        self.list.append(skill)
+    def process_skill(self):
+        """
+        list内のスキルを順次使っていく
+        """
+        for skill in self.list:
+            skill.effect()
+
+
+"""
+以下テスト
+for 文を回している最中にスキルをロードさせても
+ちゃんと処理することを確認
+"""
+
+SKILL_PROCESSOR = SkillProcessor()
+
+class skill_for_test_A:
+    def effect():
+        print("i am A and processed")
+
+
+def func_test():
+    global skill_for_test_A
+    global SKILL_PROCESSOR
+    SKILL_PROCESSOR.load_skill(skill_for_test_A)
+
+class skill_for_test_B:
+    def effect():
+        print("i am B and processed")
+        func_test()
+
+
+
+SKILL_PROCESSOR.load_skill(skill_for_test_A)
+SKILL_PROCESSOR.load_skill(skill_for_test_B)
+SKILL_PROCESSOR.process_skill()
